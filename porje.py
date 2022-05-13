3bb98f3552b43b4ebc63bbae99197f6bbf95cac7
louay alhaj ali <8hosam1998@gmail.com>
2022-05-13 16:40:30 +0300

    Create porje.py

---
+++ porje.py
@@ -0,0 +1,28 @@
+#################################################################################################
+#                      Ad & Soyad: Lüey Elhac Ali 
+#                      oğrenci numarasi : b201200554
+#                      Ödev adı : block zincri
+#################################################################################################
+import hashlib
+class LueyCoinBlock:
+ def __init__(self,previous_block_hash ,transaciton_list):
+         self.previous_block_hash=previous_block_hash
+         self.transaciton_list=transaciton_list
+      
+         self.block_data="______________".join(transaciton_list) + "______________" + previous_block_hash
+         self.block_hash=hashlib.sha256(self.block_data.encode()).hexdigest()
+    
+a1 = "luey sends 7 LC to his teacher"
+a2 = "luey sends 3.7 LC to his brother"
+a3 = "luey's teacher sends 15.2 LC to his son"
+a4 = "luey sends 100 LC to his university"
+
+initial_block = LueyCoinBlock("Initial string", [a1, a2])
+ 
+print(initial_block.block_data)
+print(initial_block.block_hash)
+
+second_block = LueyCoinBlock("Initial string", [a3, a4])
+ 
+print(second_block.block_data )
+print(second_block.block_hash)
