dc06653ec66962d86c474c387b380aaa401ee729
louay alhaj ali <8hosam1998@gmail.com>
2022-05-13 18:35:31 +0300

    Update porje.py
    commıt

--- porje.py
+++ porje.py
@@ -3,40 +3,30 @@
 #                      oğrenci numarasi : b201200554
 #                      Ödev adı : block zincri
 #################################################################################################
-import hashlib ##Programda kullanacağımız kütüphaneyi çağırdım 
-class LueyCoinBlock:##doviz adi
- def __init__(self,previous_block_hash ,transaciton_list):##önceki blok karma ve islemler listesi
-         
-         ##Her blok bir önceki bloğa bağlıdır, böylece bloklardan birinde değişiklik yapıldığında bütün de değişir.
+import hashlib
+class LueyCoinBlock:
+ def __init__(self,previous_block_hash ,transaciton_list):
          self.previous_block_hash=previous_block_hash
          self.transaciton_list=transaciton_list
       
-         self.block_data="______________".join(transaciton_list) + "______________" + previous_block_hash #her islemler ayni listede toplanir
-         self.block_hash=hashlib.sha256(self.block_data.encode()).hexdigest() # burasi verileri kodalanacak
-         
-         
-    #islemler#
+         self.block_data="______________".join(transaciton_list) + "______________" + previous_block_hash
+         self.block_hash=hashlib.sha256(self.block_data.encode()).hexdigest()
+    
 a1 = "luey sends 7 LC to his teacher"
 a2 = "luey sends 3.7 LC to his brother"
-a3 = "luey's teacher sends 15.02 LC to his son"
-a4 = "luey sends 15.021 LC to his university"
+a3 = "luey's teacher sends 15.2 LC to his son"
+a4 = "luey sends 100 LC to his university"
 
+<<<<<<< HEAD
 initial_block = LueyCoinBlock("Initial string", [a1, a2])#1.blok oluşturduk
+=======
+initial_block = LueyCoinBlock("Initial string", [a1, a2])
+>>>>>>> parent of 70c9e22 (Update porje.py)
  
-print(initial_block.block_data)#işlemler bilgileri gösterir
-print(initial_block.block_hash)#şire gösterir
+print(initial_block.block_data)
+print(initial_block.block_hash)
 
 second_block = LueyCoinBlock("Initial string", [a3, a4])
  
 print(second_block.block_data )
 print(second_block.block_hash)
-
-#Burada en basit değişiklikte şifrelemenin değişeceğini fark ediyoruz.
-third_block = LueyCoinBlock("Initial string", [a3])
- 
-print(third_block.block_data )
-print(third_block.block_hash)
-fourthly_block = LueyCoinBlock("Initial string", [a4])
- 
-print(fourthly_block.block_data )
-print(fourthly_block.block_hash)
